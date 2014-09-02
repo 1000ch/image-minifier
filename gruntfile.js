bfaab33cf837ca0840e77fdf6f065b6ff46a2a64
1000ch <shogo.sensui@gmail.com>
2014-09-02 12:52:03 +0900

    move

--- gruntfile.js
+++ gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
         dest: 'build/css/lib.css'
       },
       cssapp: {
-        src: ['index.css'],
+        src: ['src/css/index.css'],
         dest: 'build/css/app.css'
       },
       jslib: {
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
         dest: 'build/js/lib.js'
       },
       jsapp: {
-        src: ['index.js'],
+        src: ['src/js/index.js'],
         dest: 'build/js/app.js'
       }
     },
