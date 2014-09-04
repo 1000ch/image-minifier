88d5e543b2300a8cb2e5e8c091f3eacee6c0e587
1000ch <shogo.sensui@gmail.com>
2014-09-04 22:13:26 +0900

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@
 // node
 var fs = node('fs');
 var path = node('path');
+var mime = node('mime');
 
 // atom-shell
 var ipc = require('ipc');
@@ -15,14 +16,70 @@ $(function () {
     _.each(args, function (arg) {
       var stat = fs.statSync(arg);
       if (stat.isFile()) {
-        imageFileList.add(arg, {
-          type: '',
-          path: arg,
+        imageFileList.add(path.resolve(arg), {
+          type: mime.lookup(arg),
+          path: path.resolve(arg),
           name: path.basename(arg),
           size: stat.size
         });
       } else if (stat.isDirectory()) {
-        
+        _.each(fs.readdirSync(arg), function (p) {
+          var filepath = path.join(arg, p);
+          var stat = fs.statSync(filepath);
+          if (stat.isFile()) {
+            imageFileList.add(path.resolve(filepath), {
+              type: mime.lookup(filepath),
+              path: path.resolve(filepath),
+              name: path.basename(filepath),
+              size: stat.size
+            });
+          }
+        });
+
+        // render html
+        var html = '';
+        imageFileList.each(function (item) {
+          html += Mustache.render($resultItemTemplate.html(), {
+            id: item.id,
+            fileName: item.fileName,
+            beforeSizeText: item.beforeSizeText,
+            afterSizeText: '',
+            savingPercent: ''
+          });
+        });
+        $resultList.html(html);
+
+        // minify images
+        minifyImage(imageFileList.getFilePaths(), function (error, results) {
+
+          if (error) {
+            throw error;
+          }
+
+          _.each(results, function (result) {
+            var item = imageFileList.get(result.path);
+
+            // set optimized file size
+            item.afterSize = result.afterSize;
+
+            // update row
+            var $tr = $('#' + item.id);
+            $tr.find('.js-after-size').text(item.afterSizeText);
+            $tr.find('.js-saving-percent').text(item.savingPercent);
+
+            var $icon = $tr.find('.fa');
+            $icon.removeClass('fa-spinner');
+            $icon.removeClass('fa-spin');
+
+            if (item.beforeSize === item.afterSize) {
+              $icon.addClass('fa-times-circle');
+            } else {
+              $icon.addClass('fa-check');
+            }
+          });
+
+          imageFileList.clear();
+        });
       }
     });
   });
