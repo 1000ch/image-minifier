3f9389283e8e3b78993fa96cb6c5a12077bf59b1
1000ch <shogo.sensui@gmail.com>
2014-09-04 15:05:47 +0900

    update

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var app = require('app');
 var BrowserWindow = require('browser-window');
 var crashReporter = require('crash-reporter');
 
+var dialog = require('dialog');
+var ipc = require('ipc');
+
 var win = null;
 
 app.on('window-all-closed', function () {
@@ -19,10 +22,31 @@ app.on('will-finish-launching', function () {
 });
 
 app.on('finish-launching', function () {
+
+  // initialize browser window
   win = new BrowserWindow({
     width: 800,
     height: 460
   });
+
+  // handle openFileDialog event
+  ipc.on('openFileDialog', function (e, arg) {
+    dialog.showOpenDialog(win, {
+      properties: [
+        'openFile',
+        'openDirectory',
+        'multiSelections'
+      ],
+      filters: [{
+        name: 'Images',
+        extensions: ['jpg', 'png', 'gif', 'svg']
+      }]
+    }, function (arg) {
+      e.sender.send('asynchronous-reply', arg);
+    });
+  });
+
+  // open window
   win.loadUrl('file://' + __dirname + '/index.html');
 
   win.on('closed', function () {
--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,117 @@
 'use strict';
 
 var imageFileList = new ImageFileList();
+var ipc = require('ipc');
+
+/**
+ * Data Transfer Item Resolver
+ * @param {DataTransferItemList} items 
+ * @constructor
+ */
+function DataTransferItemResolver(items) {
+  this.items = items || [];
+}
+
+DataTransferItemResolver.prototype.resolve = function () {
+
+  var promises = [];
+  var forEach = Array.prototype.forEach;
+
+  forEach.call(this.items, function (item) {
+    var entry = item.webkitGetAsEntry();
+    if (entry.isFile) {
+      promises.push(new Promise(function (resolve, reject) {
+        entry.file(function (file) {
+          resolve(file);
+        });
+      }));
+    } else if (entry.isDirectory) {
+      entry.createReader().readEntries(function (fileEntries) {
+        forEach.call(fileEntries, function (fileEntry) {
+          promises.push(new Promise(function (resolve, reject) {
+            fileEntry.file(function (file) {
+              resolve(file);
+            });
+          }));
+        });
+      });
+    }
+  });
+
+  return Promise.all(promises);
+};
+
+function PathResolver(paths) {
+  this.paths = paths || [];
+}
+
+PathResolver.prototype.getFileSystem = function () {
+  return new Promise(function (resolve, reject) {
+    webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024 * 5, function (fileSystem) {
+      resolve(fileSystem);
+    }, function (fileError) {
+      reject(fileError);
+    });
+  });
+};
+
+PathResolver.prototype.resolve = function () {
+
+  var that = this;
+  var forEach = Array.prototype.forEach;
+  var endsWith = function (value, position) {
+    return (this.lastIndexOf(value, position) === (position >= 0 ? position | 0 : this.length - 1));
+  };
+
+  return new Promise(function (resolve, reject) {
+
+    that.getFileSystem().then(function (fileSystem) {
+      var promises = [];
+      forEach.call(that.paths, function (path) {
+        if (endsWith.call(path, '.png') || endsWith.call(path, '.jpg') ||
+          endsWith.call(path, '.gif') || endsWith.call(path, '.svg')) {
+          promises.push(new Promise(function (res, rej) {
+            fileSystem.root.getFile(path, {}, function (fileEntry) {
+              fileEntry.file(function (file) {
+                res(file);
+              });
+            });
+          }));
+        } else {
+          fileSystem.root.getDirectory(path, {}, function (directoryEntry) {
+            directoryEntry.createReader().readEntries(function (fileEntries) {
+              forEach.call(fileEntries, function (fileEntry) {
+                promises.push(new Promise(function (res, rej) {
+                  fileEntry.file(function (file) {
+                    res(file);
+                  });
+                }));
+              });
+            });
+          });
+        }
+      });
+
+      Promise.all(promises).then(function (files) {
+        resolve(files);
+      });
+    });
+
+  });
+};
 
 $(function () {
+
+  ipc.on('asynchronous-reply', function (args) {
+    var pathResolver = new PathResolver(args);
+    pathResolver.resolve().then(function (files) {
+      console.log(files);
+    });
+  });
+  var $descriptionButton = $('#js-description__button'); 
+  $descriptionButton.on('click', function () {
+    ipc.sendSync('openFileDialog');
+  });
 
   var $dropArea = $('#js-drop-area');
   var $dashedBorder = $('.dashed-border');
@@ -35,31 +144,9 @@ $(function () {
     e.stopPropagation();
     e.preventDefault();
 
-    // get dropping files
-    var promises = [];
-
-    _.each(e.originalEvent.dataTransfer.items || [], function (item) {
-      var entry = item.webkitGetAsEntry();
-      if (entry.isFile) {
-        promises.push(new Promise(function (resolve, reject) {
-          entry.file(function (file) {
-            resolve(file);
-          });
-        }));
-      } else if (entry.isDirectory) {
-        entry.createReader().readEntries(function (fileEntries) {
-          _.each(fileEntries, function (fileEntry) {
-            promises.push(new Promise(function (resolve, reject) {
-              fileEntry.file(function (file) {
-                resolve(file);
-              });
-            }));
-          });
-        });
-      }
-    });
-
-    Promise.all(promises).then(function (files) {
+    var items = e.originalEvent.dataTransfer.items;
+    var itemResolver = new DataTransferItemResolver(items);
+    itemResolver.resolve().then(function (files) {
 
       // when complete to fetch files and directories data
       _.each(files, function (file) {
