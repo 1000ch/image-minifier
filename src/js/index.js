5c8b1a282b19229e1fb60f899d108849b07f0638
1000ch <shogo.sensui@gmail.com>
2014-09-04 19:02:52 +0900

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -1,114 +1,29 @@
-//@depend lib/generate-id.js
-//@depend lib/minify-image.js
-
 'use strict';
 
-var imageFileList = new ImageFileList();
+// node
+var fs = node('fs');
+var path = node('path');
+
+// atom-shell
 var ipc = require('ipc');
 
-/**
- * Data Transfer Item Resolver
- * @param {DataTransferItemList} items 
- * @constructor
- */
-function DataTransferItemResolver(items) {
-  this.items = items || [];
-}
-
-DataTransferItemResolver.prototype.resolve = function () {
-
-  var promises = [];
-  var forEach = Array.prototype.forEach;
-
-  forEach.call(this.items, function (item) {
-    var entry = item.webkitGetAsEntry();
-    if (entry.isFile) {
-      promises.push(new Promise(function (resolve, reject) {
-        entry.file(function (file) {
-          resolve(file);
-        });
-      }));
-    } else if (entry.isDirectory) {
-      entry.createReader().readEntries(function (fileEntries) {
-        forEach.call(fileEntries, function (fileEntry) {
-          promises.push(new Promise(function (resolve, reject) {
-            fileEntry.file(function (file) {
-              resolve(file);
-            });
-          }));
-        });
-      });
-    }
-  });
-
-  return Promise.all(promises);
-};
-
-function PathResolver(paths) {
-  this.paths = paths || [];
-}
-
-PathResolver.prototype.getFileSystem = function () {
-  return new Promise(function (resolve, reject) {
-    webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024 * 5, function (fileSystem) {
-      resolve(fileSystem);
-    }, function (fileError) {
-      reject(fileError);
-    });
-  });
-};
-
-PathResolver.prototype.resolve = function () {
-
-  var that = this;
-  var forEach = Array.prototype.forEach;
-  var endsWith = function (value, position) {
-    return (this.lastIndexOf(value, position) === (position >= 0 ? position | 0 : this.length - 1));
-  };
-
-  return new Promise(function (resolve, reject) {
-
-    that.getFileSystem().then(function (fileSystem) {
-      var promises = [];
-      forEach.call(that.paths, function (path) {
-        if (endsWith.call(path, '.png') || endsWith.call(path, '.jpg') ||
-          endsWith.call(path, '.gif') || endsWith.call(path, '.svg')) {
-          promises.push(new Promise(function (res, rej) {
-            fileSystem.root.getFile(path, {}, function (fileEntry) {
-              fileEntry.file(function (file) {
-                res(file);
-              });
-            });
-          }));
-        } else {
-          fileSystem.root.getDirectory(path, {}, function (directoryEntry) {
-            directoryEntry.createReader().readEntries(function (fileEntries) {
-              forEach.call(fileEntries, function (fileEntry) {
-                promises.push(new Promise(function (res, rej) {
-                  fileEntry.file(function (file) {
-                    res(file);
-                  });
-                }));
-              });
-            });
-          });
-        }
-      });
-
-      Promise.all(promises).then(function (files) {
-        resolve(files);
-      });
-    });
-
-  });
-};
+var imageFileList = new ImageFileList();
 
 $(function () {
 
   ipc.on('asynchronous-reply', function (args) {
-    var pathResolver = new PathResolver(args);
-    pathResolver.resolve().then(function (files) {
-      console.log(files);
+    _.each(args, function (arg) {
+      var stat = fs.statSync(arg);
+      if (stat.isFile()) {
+        imageFileList.add(arg, {
+          type: '',
+          path: arg,
+          name: path.basename(arg),
+          size: stat.size
+        });
+      } else if (stat.isDirectory()) {
+        
+      }
     });
   });
   var $descriptionButton = $('#js-description__button'); 
@@ -150,7 +65,12 @@ $(function () {
 
       // when complete to fetch files and directories data
       _.each(files, function (file) {
-        imageFileList.add(file.path, file);
+        imageFileList.add(file.path, {
+          type: file.type,
+          path: file.path,
+          name: file.name,
+          size: file.size
+        });
       });
 
       // render html
@@ -167,7 +87,7 @@ $(function () {
       $resultList.html(html);
 
       // minify images
-      minifyImage(imageFileList.getAllFiles(), function (error, results) {
+      minifyImage(imageFileList.getFilePaths(), function (error, results) {
 
         if (error) {
           throw error;
